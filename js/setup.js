0593e0c418ee1b10a1d1157552772bec13717d44
MarvinMuwz <66476979+muwz@users.noreply.github.com>
2021-07-11 22:04:38 -0300

    Initial commit

---
+++ js/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        426:{
+            items:2
+        },
+        1024:{
+            items:3
+        }
+    }
+})
